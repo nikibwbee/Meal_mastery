bb63d59a2238f792fc44fa227710293e55996eff
MukeshMW1 <MukeshBhattaMPB111@gmail.com>
2025-03-14 12:23:47 +0545

    next auth

--- Meal_Mastery_Next_JS/src/app/api/auth/[...nextauth]/route.ts
+++ Meal_Mastery_Next_JS/src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import FacebookProvider from "next-auth/providers/facebook";
-
-export const authOptions:NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    }),
-  ],
-};
+import NextAuth from "next-auth";
+import { authOptions } from "./options"; // Ensure this path is correct
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };  // Ensures both GET and POST are handled
+export { handler as GET, handler as POST };
